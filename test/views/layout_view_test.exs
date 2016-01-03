d3971e2cb1240afd76c8dd0f11a291f4b0c40855
Po Chen <chenpaul914@gmail.com>
2016-01-03 11:51:31 +1100

    remove tests

--- test/views/layout_view_test.exs
+++
@@ -1,3 +0,0 @@
-defmodule LearnPhoenix.LayoutViewTest do
-  use LearnPhoenix.ConnCase, async: true
-end
--- test/views/page_view_test.exs
+++
@@ -1,3 +0,0 @@
-defmodule LearnPhoenix.PageViewTest do
-  use LearnPhoenix.ConnCase, async: true
-end
