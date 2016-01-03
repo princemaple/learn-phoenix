d4d2b6cc92c6a59c23da798714931ef57b9b34c9
Po Chen <chenpaul914@gmail.com>
2016-01-03 13:53:13 +1100

    test websockets

--- priv/static/js/app.js
+++ priv/static/js/app.js
@@ -1,3 +1,20 @@
-// for phoenix_html support, including form and button helpers
-// copy the following scripts into your javascript bundle:
-// * https://raw.githubusercontent.com/phoenixframework/phoenix_html/v2.3.0/priv/static/phoenix_html.js
+var socket = new Phoenix.Socket('/socket', {
+  logger: function(kind, msg, data) { console.log(`${kind}: ${msg}`, data); }
+});
+
+socket.connect();
+
+socket.onOpen(function(event) { console.log("OPEN", event); });
+socket.onError(function(event) { console.log("ERROR", event); });
+socket.onClose(function(event) { console.log("CLOSE", event); });
+
+var channel = socket.channel("tests:lobby", {});
+
+channel.join().receive("ignore", function() { console.log("auth error"); })
+           .receive("ok", function() { console.log("join ok"); });
+channel.onError(function() { console.log("something went wrong", e); });
+channel.onClose(function() { console.log("channel closed", e); });
+
+setTimeout(function() {
+  channel.push("new:msg", { a: 1, b: 2 });
+}, 2000);
